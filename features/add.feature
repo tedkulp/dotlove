7fbe0ee3bd3a6a56f589e34d7479d733ef6a0f20
Ted Kulp <ted@tedkulp.com>
2012-04-23 06:51:09 -0400

    Make add work
    Also, refactor the cli_steps so they make more
    sense and also properly bubble the exception up
    if a matcher fails, but still fixes the cwd.

--- features/add.feature
+++ features/add.feature
@@ -1,3 +1,4 @@
+@announce
 Feature: We can add a file to the repository
 
   Scenario: Add a new .file to the repository
@@ -11,6 +12,7 @@ Feature: We can add a file to the repository
     And the stdout should contain ".somefile - OK"
     And a file named ".dotfiles/.somefile" should exist
     And a file named ".somefile" is symlinked to ".dotfiles/.somefile"
+    And a file named ".somefile" is staged to be committed in ".dotfiles"
     When I run `dotlove add .somefile`
     Then the stdout should contain "File(s) added to repository"
     And the stdout should contain ".somefile - Already Added"
--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -3,34 +3,77 @@ Given /^a directory named "([^"]*)" doesn't exist$/ do |directory|
 end
 
 Then /^"([^"]*)" is a valid git repository$/ do |directory|
-  pwd = Dir.pwd
-  dirs = @dirs
-  cd(directory)
-  Dir.chdir(current_dir)
-  %x(git rev-parse --git-dir).chomp.should == '.git'
-  Dir.chdir(pwd)
-  @dirs = dirs
+  in_subdirectory(directory) do
+    %x(git rev-parse --git-dir).chomp.should == '.git'
+  end
 end
 
 Then /^"([^"]*)" is not a valid git repository$/ do |directory|
+  in_subdirectory(directory) do
+    %x(git rev-parse --git-dir).chomp.should_not == '.git'
+  end
+end
+
+Then /^a file named "([^"]*)" is symlinked to "([^"]*)"$/ do |arg1, arg2|
+  in_directory do
+    File.readlink(arg1).should == arg2
+  end
+end
+
+Given /^a working repository$/ do
+  run_simple("dotlove init")
+end
+
+Then /^a file named "([^"]*)" is staged to be committed in "([^"]*)"$/ do |file, directory|
+  in_subdirectory(directory) do
+    %x(git status --porcelain).chomp.should match /A\ +#{file}/mi
+  end
+end
+
+# Run a block of code in current Aruba test
+# directory. Makes sure that # it's reset back
+# to the proper Aruba test # directory before
+# exiting the block
+#
+# block - The block of code to run in the context
+#         of the Aruba test directory
+#
+# Returns nothing
+def in_directory(&block)
+  pwd = Dir.pwd
+  dirs = @dirs
+  Dir.chdir(current_dir)
+  begin
+    yield
+  rescue Exception => e
+    raise e
+  ensure
+    Dir.chdir(pwd)
+    @dirs = [dirs[0]]
+  end
+end
+
+# Run a block of code in a subdirectory of the
+# current Aruba test directory. Makes sure that
+# it's reset back to the proper Aruba test
+# directory before exiting the block
+#
+# directory - The subdirectory to switch to
+# block     - The block of code to run in the context
+#             of the given subdirectory
+#
+# Returns nothing
+def in_subdirectory(directory, &block)
   pwd = Dir.pwd
   dirs = @dirs
   cd(directory)
   Dir.chdir(current_dir)
-  %x(git rev-parse --git-dir).chomp.should_not == '.git'
-  Dir.chdir(pwd)
-  @dirs = dirs
-end
-
-Then /^a file named "([^"]*)" is symlinked to "([^"]*)"$/ do |arg1, arg2|
-  pwd = Dir.pwd
-  dirs = @dirs
-  Dir.chdir(current_dir)
-  File.readlink(arg1).should == arg2
-  Dir.chdir(pwd)
-  @dirs = dirs
-end
-
-Given /^a working repository$/ do
-  run_simple("bundle exec dotlove init")
+  begin
+    yield
+  rescue Exception => e
+    raise e
+  ensure
+    Dir.chdir(pwd)
+    @dirs = [dirs[0]]
+  end
 end
